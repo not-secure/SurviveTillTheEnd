d0683673fbbd5fa3475dbd92501e297e2f333640
fi <litmify@gmail.com>
2020-11-23 01:30:16 +0900

    WIP

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public enum DayStatus { Day, Night };
+
+    public int time;
+    public int changePeriod = 100;
+    public DayStatus dayStatus;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+            StartGame();
+    }
+
+    public void StartGame()
+    {
+        time = 0;
+        dayStatus = DayStatus.Day;
+
+        StartCoroutine(UpdateGame());
+        Debug.Log("Starting game...");
+    }
+
+    public void ChangeDayStatus()
+    {
+        if (dayStatus == DayStatus.Day)
+            dayStatus = DayStatus.Night;
+        else
+            dayStatus = DayStatus.Day;
+    }
+
+    public IEnumerator UpdateGame()
+    {
+        while (true)
+        {
+            time += 1;
+
+            if (time % changePeriod == 0)
+                ChangeDayStatus();
+
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
