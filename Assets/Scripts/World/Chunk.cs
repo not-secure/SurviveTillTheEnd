fa991d147eeee41f0752d31e33ba4a103ed8b826
Khinenw <deu07115@gmail.com>
2020-12-07 16:31:36 +0900

    feat(block): Add chunk template 5,6,7

--- Assets/Scripts/World/Chunk.cs
+++ Assets/Scripts/World/Chunk.cs
@@ -1,52 +1,50 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using Block;
+﻿using Block;
 using UnityEngine;
-using World;
 
-[System.Serializable]
-public class Chunk
-{
-    public Vector2Int Position;
-    public int type;
-    public GameObject gameObject;
+namespace World {
+    [System.Serializable]
+    public class Chunk {
+        public Vector2Int Position;
+        public int type;
+        public GameObject gameObject;
 
-    private int _variant;
-    private ChunkController _chunkController;
+        private int _variant;
+        private ChunkController _chunkController;
     
-    public Chunk(Vector2Int position, int type) {
-        this.Position = position;
-        this.type = type;
+        public Chunk(Vector2Int position, int type) {
+            this.Position = position;
+            this.type = type;
 
-        _variant = Random.Range(1, 5);
-    }
+            _variant = Random.Range(1, 7);
+        }
 
-    public void Generate() {
-        if (gameObject) return;
+        public void Generate() {
+            if (gameObject) return;
         
-        gameObject = Object.Instantiate(
-            Resources.Load<GameObject>("Chunks/" + type.ToString() + "_type/" + _variant + "_num")
-        );
+            gameObject = Object.Instantiate(
+                Resources.Load<GameObject>("Chunks/" + type.ToString() + "_type/" + _variant + "_num")
+            );
 
-        _chunkController = gameObject.GetComponent<ChunkController>();
-        gameObject.transform.position = new Vector3(
-            Position.x * WorldManager.ChunkSize.x, 
-            0, 
-            Position.y * WorldManager.ChunkSize.y
-        );
-    }
+            _chunkController = gameObject.GetComponent<ChunkController>();
+            gameObject.transform.position = new Vector3(
+                Position.x * WorldManager.ChunkSize.x, 
+                0, 
+                Position.y * WorldManager.ChunkSize.y
+            );
+        }
 
-    public void Unload() {
-        _chunkController = null;
+        public void Unload() {
+            _chunkController = null;
         
-        Object.Destroy(gameObject);
-        gameObject = null;
-    }
+            Object.Destroy(gameObject);
+            gameObject = null;
+        }
 
-    public BlockController[] ActiveBlocks() {
-        if (!_chunkController)
-            return new BlockController[] {};
+        public BlockController[] ActiveBlocks() {
+            if (!_chunkController)
+                return new BlockController[] {};
         
-        return _chunkController.AvailableBlock;
+            return _chunkController.AvailableBlock;
+        }
     }
 }
