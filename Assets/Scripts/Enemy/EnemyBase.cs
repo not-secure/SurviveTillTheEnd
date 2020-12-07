7480188c6fcfe5a06cb91138d83cc85794b7ce1a
fi <litmify@gmail.com>
2020-12-07 17:39:46 +0900

    Add simple enemy

---
+++ Assets/Scripts/Enemy/EnemyBase.cs
@@ -0,0 +1,16 @@
+﻿using Player;
+using UnityEngine;
+
+namespace Enemy {
+    public class EnemyBase : MonoBehaviour
+    {
+        public BoxCollider boxCollider;
+        public GameObject player;
+
+        public void Awake()
+        {
+            boxCollider = GetComponent<BoxCollider>();
+            player = FindObjectOfType<PlayerController>().gameObject;
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy/EnemySample.cs
@@ -0,0 +1,23 @@
+﻿using Enemy;
+using UnityEngine;
+
+public class EnemySample : EnemyBase
+{
+    private float startY;
+
+    public void Start()
+    {
+        startY = transform.position.y;   
+    }
+
+    public void Update()
+    {
+        // Moving towards player
+        Quaternion playerRotation = Quaternion.LookRotation(player.transform.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, playerRotation, Time.deltaTime * 5);
+        transform.position += transform.forward * Time.deltaTime * 10;
+
+        // Simple floating effect
+        transform.position = new Vector3(transform.position.x, startY, transform.position.z);
+    }
+}
